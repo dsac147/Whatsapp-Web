e4f9bcb915a21f29a3a0406b09692760008d2320
Divyanshu Sachan <dsac147@gmail.com>
2020-09-13 18:46:40 +0530

    Basic Frontend Complete

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Chat.css";
 import {Avatar , IconButton} from '@material-ui/core';
-import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import ChatIcon from '@material-ui/icons/Chat';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import MicIcon from '@material-ui/icons/Mic';
 
 function Chat() {
+    var d = new Date()
     return (
         <div className="chat">
             <div className="chat__header">
@@ -17,25 +19,46 @@ function Chat() {
                 </div>
                 <div className="chat__headerRight">
                     <IconButton>
-                        <DonutLargeIcon/>
+                        <SearchOutlinedIcon/>
                     </IconButton>
                     <IconButton>
-                        <ChatIcon/>
+                        <AttachFileIcon/>
                     </IconButton>
                     <IconButton>
                         <MoreVertIcon/>
                     </IconButton>
                 </div>
-                <div className="chat__body">
-                    <p>
-                        <span className="chat__name">Sonny</span>
-                        This is a message
-                        <span className="chat__timestamp">
-                            {new.Date().toUTCString()}
-                        </span>
-
-                    </p>
-                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {d.getHours()}:{d.getMinutes()}
+                    </span>
+                </p>
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {d.getHours()}:{d.getMinutes()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Sonny</span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {d.getHours()}:{d.getMinutes()}
+                    </span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon/>
+                <form>
+                    <input type="text" placeholder="Type a message..."/>
+                    <button type="submit">Sens a message</button>
+                </form>
+                <MicIcon/>
             </div>
         </div>
     )
